950279c341aafe424a6b61f9568a6aa274fa3fef
Farouq-beginner <24111814081@mhs.unesa.ac.id>
2025-10-31 21:09:20 +0700

    Update UpdateController.php

--- app/Http/Controllers/UpdateController.php
+++ app/Http/Controllers/UpdateController.php
@@ -9,12 +9,12 @@ class UpdateController extends Controller
     public function checkUpdate(Request $request)
     {
         $currentBuild = $request->query('build_number', 1); // build number dari aplikasi
-        $latestBuild = 4; // build number terbaru di server
+        $latestBuild = 3; // build number terbaru di server
 
         $forceUpdate = $currentBuild < $latestBuild; // wajib update kalau build lama
 
         return response()->json([
-            'latest_version' => '1.0.1',
+            'latest_version' => '1.0.2',
             'latest_build' => $latestBuild,
             'update_required' => $forceUpdate,
             'download_url' => 'https://drive.google.com/drive/folders/1--6Q3oPzp7kHEWJocgfOIZzxbsMukOxo?usp=sharing',

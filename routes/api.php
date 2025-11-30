d78b789d48a31f9fb8087c976a704e6d35564b3c
Farouq-beginner <24111814081@mhs.unesa.ac.id>
2025-11-30 17:12:07 +0700

    Clear Cache

--- routes/api.php
+++ routes/api.php
@@ -34,6 +34,12 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
+Route::get('/clear-cache', function() {
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    return "Cache Cleared";
+});
+
 // --- RUTE PUBLIK ---
 Route::get('/server/status', [ServerController::class, 'checkStatus']);
 
